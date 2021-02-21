4a7d1055e058a7ffaa7b35ee884b505d0b733bed
VENKAT <VENKAT@LAPTOP-IDRICAAA>
2021-02-21 16:27:52 +0530

    10th

--- vk/src/main/java/Test/vk/sample.java
+++ vk/src/main/java/Test/vk/sample.java
@@ -2,6 +2,7 @@ package Test.vk;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -11,11 +12,11 @@ WebDriver driver;
 	
 	@Test
 	public void launch() throws InterruptedException {
-//	System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32 (5)\\chromedriver.exe");  
-//	 WebDriver driver =new ChromeDriver(); 
+	System.setProperty("webdriver.chrome.driver","D:\\Driver\\Chrome\\chromedriver.exe");  
+    driver =new ChromeDriver(); 
 	 
-	 System.setProperty("webdriver.gecko.driver", "E:\\Gecko\\chromedriver_win32\\FF\\geckodriver.exe");
-	    driver = new FirefoxDriver();
+//	 System.setProperty("webdriver.gecko.driver", "E:\\Gecko\\chromedriver_win32\\FF\\geckodriver.exe");
+	//    driver = new FirefoxDriver();
 //	 System.setProperty("webdriver.ie.driver","E:\\IEDriverServer_x64_2.48.0\\IEDriverServer.exe");
 //	 WebDriver driver = new InternetExplorerDriver();
 	 driver.manage().window().maximize();
